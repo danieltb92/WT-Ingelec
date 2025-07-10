45035afc33658d55780cfe5cf6137e64f13dcfa8
Daniel Tunjano <daniel.tunjano92@gmail.com>
2025-07-10 11:34:01 -0500

    Add Astro Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // Import the Vercel adapter
 // import vercel from '@astrojs/vercel';
@@ -8,15 +10,14 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wtingelec.com.co",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   // output: 'server',
   // adapter: vercel(),
   // vite: {
   //   server: {
   //     allowedHosts:
   //       ['all',
-  //       'million-ext-cult-affiliation.trycloudflare.com'] // This allows the server to accept requests from any host, which is useful for development environments.   
+  //       'million-ext-cult-affiliation.trycloudflare.com'] // This allows the server to accept requests from any host, which is useful for development environments.
   //   }
   // }
 });
-
