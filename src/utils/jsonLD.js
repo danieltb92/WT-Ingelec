dc85e204aac95b91789c2b2a75b121e11e20a653
Daniel Tunjano <danieltb92@hotmail.com>
2026-02-10 16:33:20 -0500

    feat: Introduce JSON-LD structured data generation for SEO-Sitelinks google

--- src/utils/jsonLD.js
+++ src/utils/jsonLD.js
@@ -2,34 +2,61 @@ import siteData from "../data/siteData.json";
 import { slugify } from "./slugify";
 
 export default function jsonLDGenerator({ type, post, url }) {
-  // If the page has CMS data, use it.
+  const schemas = [];
+
+  // Organization Schema (Global)
+  schemas.push({
+    "@type": "Organization",
+    "name": siteData.organization.name,
+    "legalName": siteData.organization.legalName,
+    "url": siteData.organization.url,
+    "logo": siteData.organization.logo,
+    "contactPoint": {
+      "@type": "ContactPoint",
+      "telephone": siteData.organization.contactPoint.telephone,
+      "contactType": siteData.organization.contactPoint.contactType,
+      "email": siteData.organization.contactPoint.email,
+      "areaServed": siteData.organization.contactPoint.areaServed,
+      "availableLanguage": siteData.organization.contactPoint.availableLanguage
+    },
+    "sameAs": siteData.organization.sameAs
+  });
+
+  // WebSite Schema (Global or Home)
+  schemas.push({
+    "@type": "WebSite",
+    "name": siteData.title,
+    "url": siteData.siteUrl,
+    "description": siteData.description,
+    "publisher": {
+      "@id": `${siteData.siteUrl}#organization`
+    }
+  });
+
+  // BlogPosting Schema (Conditional)
   if (type === "post") {
-    return `<script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "BlogPosting",
-        "mainEntityOfPage": {
-          "@type": "WebPage",
-          "@id": "${url}"
-        },
-        "headline": "${post.title}",
-        "description": "${post.description}",
-        "image": "${post.image.src}",
-        "author": {
-          "@type": "Person",
-          "name": "${post.author}",
-          "url": "/author/${slugify(post.author)}"
-        },
-        "datePublished": "${post.date}"
-      }
-    </script>`;
+    schemas.push({
+      "@type": "BlogPosting",
+      "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": url
+      },
+      "headline": post.title,
+      "description": post.description,
+      "image": post.image.src,
+      "author": {
+        "@type": "Person",
+        "name": post.author,
+        "url": `/author/${slugify(post.author)}`
+      },
+      "datePublished": post.date
+    });
   }
+
   return `<script type="application/ld+json">
-      {
-      "@context": "https://schema.org/",
-      "@type": "WebSite",
-      "name": "${siteData.title}",
-      "url": "${import.meta.env.SITE}"
-      }
-    </script>`;
+    ${JSON.stringify({
+      "@context": "https://schema.org",
+      "@graph": schemas
+    })}
+  </script>`;
 }
